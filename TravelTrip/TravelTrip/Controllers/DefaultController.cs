fbd72454a1c78938efd92ff98168a1a74a44cf04
beratozguur <beratozguur06@gmail.com>
2024-06-13 12:17:49 +0300

    TravelTrip
    ASP .NET'te Entity Framework Code First yaklaşımıyla geliştirilen, admin panelli bir MVC projesi olan tatil seyahat sitesi.

---
+++ TravelTrip/TravelTrip/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTrip.Models.Sınıflar;
+
+namespace TravelTrip.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.Take(10).ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+
+            return PartialView(deger);
+        }
+        public ActionResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
+            return PartialView(deger);
+        }
+        public ActionResult Iletisim()
+        {
+            var ilet = c.iletisims.ToList();
+            return View(ilet);
+        }
+        public ActionResult Fotograflar()
+        {
+            var foto = c.Blogs.ToList();
+            return View(foto);
+        }
+    }
+}
